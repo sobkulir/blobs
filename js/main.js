1787363b2a08f3128b8cfa864f4b8079320acdd5
Roman Sobkuliak <r.sobkuliak@gmail.com>
2023-01-29 14:19:03 +0100

    Inject p5 into container

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
+let CANVAS = null;
+const CANVAS_ID = "main-canvas";
+
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+    const el = document.getElementById(CANVAS_ID);
+    CANVAS = createCanvas(el.clientWidth, el.clientHeight);
+    CANVAS.parent(CANVAS_ID);
 }
 
 function draw() {
-    background(255);
+    background(200);
     text("put your p5.js code here",10, frameCount % height);
 }
 
 function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
+    const el = document.getElementById(CANVAS_ID);
+    resizeCanvas(el.clientWidth, el.clientHeight);
 }
